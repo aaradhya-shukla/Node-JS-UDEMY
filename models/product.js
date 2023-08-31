ab757a45d4c16af5079a6a421da048e5270a964a
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-08-31 18:28:54 +0530

    added deletion and editing functionalities

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id=id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,13 +27,37 @@ module.exports = class Product {
   }
 
   save() {
-    this.id=Math.random().toString();
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      getProductsFromFile(products => {
+        if (this.id){
+          const existingProductIndex=products.findIndex(p=>p.id===this.id)
+          const updatedProducts=[...products];
+          updatedProducts[existingProductIndex]=this;
+          fs.writeFile(p,JSON.stringify(updatedProducts),err=>{
+            console.log(err)
+          })
+        }
+        else{
+          this.id=Math.random().toString();
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+            });
+        }
+      
     });
+    
+  }
+
+  static delete(id) {
+     getProductsFromFile(products=>{
+      const index=products.findIndex(p=>p.id===id)
+      const updatedProducts=[...products]
+      updatedProducts.splice(index,index+1);
+      console.log(updatedProducts);
+      fs.writeFile(p,JSON.stringify(updatedProducts),(err)=>{
+        console.log(err)
+      })
+    })
   }
 
   static fetchAll(cb) {
--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,18 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+// /admin/edit-prodcut/dynamic => GET
+
+
+
+router.get('/edit-product/:productId',adminController.getEditProduct);
+
+// /admin/edit-product =>POST
+
+router.post('/edit-product',adminController.postEditProduct);
+
+// admin/delete-product =>
+
+router.post('/delete-product/:productId',adminController.deleteProduct);
+
 module.exports = router;
